5a565e5034e3ca409ef5d385a466edabeb3a7151
Alex Dorosin <alexdorosin@gmail.com>
2025-06-01 16:05:50 +0300

    add firebase config

---
+++ src/firebase.ts
@@ -0,0 +1,28 @@
+import { initializeApp, type FirebaseApp } from "firebase/app";
+import { getFirestore, type Firestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID, // Optional
+};
+
+let app: FirebaseApp;
+let firestore: Firestore;
+
+try {
+  app = initializeApp(firebaseConfig);
+  firestore = getFirestore(app);
+
+  console.log("Firebase initialized successfully using environment variables.");
+} catch (error) {
+  console.error("Firebase initialization failed.", error);
+  document.body.innerHTML = `<h1>Firebase Initialization Error</h1>`;
+  throw error;
+}
+
+export { app, firestore };
