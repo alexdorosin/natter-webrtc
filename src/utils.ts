8e3597523e7c8ca5c62d029ee6aeddcb3e60ffa6
Alex Dorosin <alexdorosin@gmail.com>
2025-06-07 16:05:44 +0300

    refactor webrtc handlers

--- src/utils.ts
+++ src/utils.ts
@@ -7,3 +7,27 @@ export function getElement<T extends HTMLElement>(id: string): T {
 
   return element;
 }
+
+export function addLocalTracksToPC(
+  pc: RTCPeerConnection | null,
+  localStream: MediaStream | null
+): void {
+  if (!pc || !localStream) return;
+
+  localStream.getTracks().forEach((track) => {
+    pc.addTrack(track, localStream);
+  });
+}
+
+export function setupPeerConnectionListeners(
+  pc: RTCPeerConnection | null,
+  remoteStream: MediaStream | null
+): void {
+  if (!pc || !remoteStream) return;
+
+  pc.ontrack = (event) => {
+    event.streams[0]
+      .getTracks()
+      .forEach((track) => remoteStream.addTrack(track));
+  };
+}
